c2c6833920b22dd0d9b65eb631ec2be9b400a554
Kayque Estevão <kayque2911@gmail.com>
2025-03-07 17:33:22 -0300

    Usando if e else e operadores relacionais

--- aula2/script.js
+++ aula2/script.js
@@ -3,32 +3,38 @@ function adicionarTarefa() {
     // pega o id do input
     let inputTarefa = document.getElementById('inputTarefa')
     // pega o valor do input
-    let tarefa = inputTarefa.value;
-    
-    // se digitar algo faça
-    if (tarefa != num || tarefa != ""){
+    let tarefa = inputTarefa.value.trim();
+    //pega o valor do p
+    const mensagem = document.getElementById('mensagem')
+
+    // se o valor for vazio retorna erro
+
+    if (tarefa == ""){
+        let mensagemErro = "Digite uma tarefa para adicioná-la a sua lista!"
+        mensagem.textContent = mensagemErro
+        mensagem.style.color = "#A34743"
+  
+    }else{
         // mensagem de sucesso
-        let mensagem = "Tarefa adicionada com sucesso!";
+        let mensagemSucesso = "Tarefa adicionada com sucesso!";
         // pega a mensagem e manda pro parágrafo    
-        document.getElementById('mensagem').textContent = mensagem
+        mensagem.textContent = mensagemSucesso
+        mensagem.style.color = "#28A745"
+        
         // pega o id do ul
-        let listaTarefas = document.getElementById('listaTarefas')
+        const listaTarefas = document.getElementById('listaTarefas')
         // cria uma item da lista
         let novaTarefa = document.createElement('li')
         // adiciona o valor do input na lista
         novaTarefa.textContent = tarefa
-        
-        // coloco a minha lista dentro do ul
-        listaTarefas.appendChild(novaTarefa)
+         // coloco a minha lista dentro do ul
+        listaTarefas.appendChild(novaTarefa)     
+  }       
         // limpa o input
         inputTarefa.value = ""
-    }
-    // se for digitado nada
-    else{
-        // mensagem de erro
-       let mensagem = "Erro, você não digitou nada!"
-       // manda mensagem pro parágrafo
-       document.getElementById('mensagem').textContent = mensagem
-    }
+  
 }
+  
+  
+
   
