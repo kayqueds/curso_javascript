4cca56ef3137bf46d5f9e6c0c2c2e757f2b35e73
Kayque Estevão <kayque2911@gmail.com>
2025-03-10 16:02:08 -0300

    Adicionando vetor e loops para poder guardar os items na lista

--- aula2/script.js
+++ aula2/script.js
@@ -1,13 +1,12 @@
 // código javascript
+
+let tarefas = []
 function adicionarTarefa() {
-    // pega o id do input
-    let inputTarefa = document.getElementById('inputTarefa')
-    // pega o valor do input
+    
+    const inputTarefa = document.getElementById('inputTarefa')
     let tarefa = inputTarefa.value.trim();
-    //pega o valor do p
     const mensagem = document.getElementById('mensagem')
 
-    // se o valor for vazio retorna erro
 
     if (tarefa == ""){
         let mensagemErro = "Digite uma tarefa para adicioná-la a sua lista!"
@@ -15,26 +14,27 @@ function adicionarTarefa() {
         mensagem.style.color = "#A34743"
   
     }else{
-        // mensagem de sucesso
         let mensagemSucesso = "Tarefa adicionada com sucesso!";
-        // pega a mensagem e manda pro parágrafo    
         mensagem.textContent = mensagemSucesso
         mensagem.style.color = "#28A745"
-        
-        // pega o id do ul
-        const listaTarefas = document.getElementById('listaTarefas')
-        // cria uma item da lista
-        let novaTarefa = document.createElement('li')
-        // adiciona o valor do input na lista
-        novaTarefa.textContent = tarefa
-         // coloco a minha lista dentro do ul
-        listaTarefas.appendChild(novaTarefa)     
+        tarefas.push(tarefa)
+        renderizarTarefas()
   }       
-        // limpa o input
         inputTarefa.value = ""
-  
 }
-  
+// adicionando função de renderizar
+function renderizarTarefas(){
+    const listaTarefas = document.getElementById('listaTarefas')
+   
+    listaTarefas.innerHTML = ""
+    let i = 0
+    for(i; i < tarefas.length; i++ ){
+        let novaTarefa = document.createElement('li')
+        novaTarefa.textContent = tarefas[i]
+        listaTarefas.appendChild(novaTarefa)  
+    }
+       
+}
   
 
   
