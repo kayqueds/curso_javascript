98b140609a1592b186085d07bf594e2347839af9
Kayque Estevão <kayque2911@gmail.com>
2025-03-11 17:38:16 -0300

    Adicionando função de excluir e editar lista

--- aula2/script.js
+++ aula2/script.js
@@ -6,11 +6,13 @@ function adicionarTarefa() {
     const inputTarefa = document.getElementById('inputTarefa')
     let tarefa = inputTarefa.value.trim();
     const mensagem = document.getElementById('mensagem')
+   
 
 
     if (tarefa == ""){
+      
         let mensagemErro = "Digite uma tarefa para adicioná-la a sua lista!"
-        mensagem.textContent = mensagemErro
+        my5t5ensagem.textContent = mensagemErro
         mensagem.style.color = "#A34743"
   
     }else{
@@ -27,14 +29,57 @@ function renderizarTarefas(){
     const listaTarefas = document.getElementById('listaTarefas')
    
     listaTarefas.innerHTML = ""
-    let i = 0
-    for(i; i < tarefas.length; i++ ){
+ 
+    for(let i = 0; i < tarefas.length; i++ ){
         let novaTarefa = document.createElement('li')
         novaTarefa.textContent = tarefas[i]
+        // criando botao remover
+        let botaoRemover = document.createElement('button')
+        botaoRemover.className = 'remover'
+        botaoRemover.textContent = "Remover"
+        botaoRemover.onclick = () => removerTarefa(i)
+       
+
+        let botaoEditar = document.createElement('button')
+        botaoEditar.className = "editar"
+        botaoEditar.textContent = "Editar"
+        botaoEditar.onclick = () => editarTarefa(i)
+        limparLista()
+
+
+        novaTarefa.appendChild(botaoRemover)
+        novaTarefa.appendChild(botaoEditar)
         listaTarefas.appendChild(novaTarefa)  
     }
-       
+
+
+
+        // criandoa lógica de apgar
+       function removerTarefa(i){
+            tarefas.splice(i, 1)
+            renderizarTarefas()
+       }
+
+       // criando a lógica de editar
+
+       function editarTarefa(i){
+        let tarefaEditada = prompt("Edite a sua tarefa: ")
+        if (tarefaEditada.trim() !== ""){
+            tarefas[i] = tarefaEditada
+            renderizarTarefas()
+        }
+    }
+
+     
 }
   
-
-  
+/* 
+    criando uma função de click que limpa
+*/
+function limparLista(){
+    
+    tarefas.length = 0
+    const mensagem = document.getElementById('mensagem')
+    mensagem.textContent = "Lista de tarefas limpa com sucesso"
+    renderizarTarefas()
+   }
